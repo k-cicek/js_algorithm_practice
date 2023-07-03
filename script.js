04f393067cdd9496de1a4fea4e46f2b8bea79088
k-cicek <kevserrcicek@gmail.com>
2023-07-03 12:54:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//[8,4,6,2,3] => output => [4,2,4,2,3] --> subtraction methods
+
+const solution = (arr) => {
+    return arr.map((_,i) =>{
+        for(let j= i + 1; j < arr.length; j++){
+            if(arr[i] > arr[j]) {
+                arr[i] = arr[i] - arr[j]
+                break
+            }
+        }
+        return arr[i]
+    })
+    
+}
+
+console.log(solution([8,4,6,2,3]))
